728af8ad8332d9e06a3ead89337035bfd359dc6d
Marc Blanchard <marcblanchard0930@gmail.com>
2019-07-16 02:06:12 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,12 +1,17 @@
 def mario
-  
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
-
+mario
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+toadstool
 
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end
+link
 
